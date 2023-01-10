25d4b6eb54bc32d7047a1cf4263bea5330a5846f
adipatel11 <120439586+adipatel11@users.noreply.github.com>
2023-01-10 11:06:59 -0600

    Update main.py

--- Non-Follower Project/main.py
+++ Non-Follower Project/main.py
@@ -1,21 +1,25 @@
 import instaloader
+    
 
-# Get instance
-L = instaloader.Instaloader()
+    
+    
+def main():
+    # Get instance
+    L = instaloader.Instaloader()
 
-# Optionally, login or load session
-L.login('jdonne233', 'hello'*3)
+    # Optionally, login or load session
+    L.login('burnerUsername', 'burnerPassword')
 
-searchee = 'johndavidhagood'
+    searchee = 'username_of_profile_of_interest'
 
-profile = instaloader.Profile.from_username(L.context, searchee)
+    profile = instaloader.Profile.from_username(L.context, searchee) # get user profile
 
-followers = set(profile.get_followers())
+    followers = set(profile.get_followers()) # get both followers and followees
 
-followees = set(profile.get_followees())
+    followees = set(profile.get_followees())
 
-intersection = followers.intersection(followees)
-non_follow_backs = list(followees - intersection)
+    intersection = followers.intersection(followees) # find the intersection and subtract from followees to find non-followers
+    non_follow_backs = list(followees - intersection)
 
-for profile in non_follow_backs:
-    print(profile)
+    for profile in non_follow_backs: # display non-followers in terminal
+        print(profile)
